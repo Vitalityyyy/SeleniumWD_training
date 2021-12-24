ad297756b5e9f5534b834aa54118e04a6818d418
Vita <vita_f@list.ru>
2021-12-24 13:00:59 +0300

    Task 7 StickerscheckTest was added

---
+++ SelemiumWD2_litecart/SelemiumWD2_litecart/T4_7_Stickerscheck.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System.Collections.Generic;
+
+namespace SelemiumWD2_litecart
+{
+    [TestFixture]
+    public class T4_7_Stickerscheck
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new FirefoxDriver(@"C:\distr");
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            baseURL = "http://localhost/litecart";
+            verificationErrors = new StringBuilder();
+        }
+
+        [Test]
+        public void StickerscheckTest()
+        {
+            GoToHomepage();
+            CheckStickers();
+        }
+
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+        public void GoToHomepage()
+        {
+            driver.Navigate().GoToUrl("http://localhost/litecart");
+        }
+        public void CheckStickers()
+        {
+            var products = driver.FindElements(By.CssSelector("[class^=product]"));
+            foreach (IWebElement product in products)
+            {
+                Assert.IsTrue(IsElementPresent(By.CssSelector("[class^=sticker]")));
+            }
+        }
+    }
+}
+
